970c40d8cc8c0425ffb6da2a66ac5f38626f3151
aswinkumar-dj <203110173+aswinkumar-dj@users.noreply.github.com>
2025-11-19 17:53:52 +0530

    Created Static Sidebar

--- src/components/body/SideBar.jsx
+++ src/components/body/SideBar.jsx
@@ -1,7 +1,48 @@
-import React from "react";
-
 const SideBar = () => {
-  return <div>SideBar</div>;
+  const content = ["Home", "Shorts", "Subscriptions", "YouTube Music"];
+  const explore = [
+    "Trending",
+    "Shopping",
+    "Movies",
+    "Gaming",
+    "News",
+    "Sports",
+    "Courses",
+    "Podcasts",
+    "Library",
+    "History",
+  ];
+  return (
+    <div className="w-50 min-w-50 shrink-0 h-screen bg-base-200 border-r border-gray-600 p-4 overflow-y-auto no-scrollbar">
+      <div className="mb-6">
+        <ul className="space-y-1">
+          {content.map((item, index) => (
+            <li
+              key={index}
+              className="cursor-pointer hover:bg-black/30 p-2 rounded-lg font-medium text-sm"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+      <hr className="my-4 border-gray-600" />
+      <div>
+        <h1 className="text-md font-bold mb-3 text-white uppercase tracking-wide">
+          Explore
+        </h1>
+        <ul className="space-y-1">
+          {explore.map((item, index) => (
+            <li
+              key={index}
+              className="cursor-pointer hover:bg-black/30 p-2 rounded-lg font-medium text-sm"
+            >
+              {item}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
-
 export default SideBar;
